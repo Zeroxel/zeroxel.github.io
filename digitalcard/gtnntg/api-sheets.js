c282fbe9c6314e41baf8a93827bace4184094891
Zeroxel <gtx.github@mail.ru>
2024-12-26 08:08:00 +0100

    Update api-sheets.js

--- digitalcard/gtnntg/api-sheets.js
+++ digitalcard/gtnntg/api-sheets.js
@@ -4,15 +4,27 @@ async function fetchStatus() {
   try {
     const response = await fetch(apiUri_sheets);
     const data = await response.json();
-    
+
     if (data.success && data.data.length > 0) {
       const status = data.data[0].status; 
       const username = data.data[0].username;
+      const avatar = data.data[0].avatar; // Поле для аватара
+
       document.getElementById('status').textContent = `${status}`;
       document.getElementById('username').textContent = `${username}`;
+
+      const avatarElement = document.getElementById('avatar');
+      if (avatar) {
+        avatarElement.src = avatar; // Установка ссылки на аватар
+        avatarElement.alt = `Avatar of ${username}`;
+      } else {
+        console.warn('Ссылка на аватар отсутствует.');
+        avatarElement.alt = 'Аватар отсутствует';
+      }
     } else {
       document.getElementById('status').textContent = 'Статус не найден.';
       document.getElementById('username').textContent = 'Не найден.';
+      console.error('Данные для статуса и имени не найдены.');
     }
   } catch (error) {
     console.error('Ошибка при получении данных:', error);
